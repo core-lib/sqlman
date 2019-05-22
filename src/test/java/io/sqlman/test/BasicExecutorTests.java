6e9b2300aa0a1977dbab0a1667b76361aba5f3fb
core-lib <646742615>
2019-05-22 19:15:32 +0800

    脚本执行

--- src/test/java/io/sqlman/test/BasicExecutorTests.java
+++ src/test/java/io/sqlman/test/BasicExecutorTests.java
@@ -2,7 +2,11 @@ package io.sqlman.test;
 
 import com.alibaba.druid.pool.DruidDataSource;
 import io.sqlman.executor.BasicExecutor;
+import net.sf.jsqlparser.parser.CCJSqlParser;
+import net.sf.jsqlparser.parser.StreamProvider;
 import org.junit.Test;
+
+import java.io.InputStream;
 
 /**
  * @author Payne 646742615@qq.com
@@ -12,6 +16,10 @@ public class BasicExecutorTests {
 
     @Test
     public void test() throws Exception {
+        InputStream in = this.getClass().getResourceAsStream("/sqlman/v5.2.0-仓库平均成本价.sql");
+        CCJSqlParser parser = new CCJSqlParser(new StreamProvider(in));
+        parser.Statements();
+
         DruidDataSource dataSource = new DruidDataSource();
         dataSource.setUrl("jdbc:mysql://new-alpha.juniusoft.com:23306/new_mf_pro_bak_0408?useUnicode=true&characterEncoding=utf-8&useSSL=false&serverTimezone=GMT%2B8");
         dataSource.setUsername("new_mf");
