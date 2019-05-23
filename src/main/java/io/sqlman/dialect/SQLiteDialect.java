cd00b24044dd97b9e18a19e90e521edebe2e6601
core-lib <646742615>
2019-05-23 23:05:26 +0800

    切换成druid来解析SQL脚本

--- src/main/java/io/sqlman/dialect/SQLiteDialect.java
+++ src/main/java/io/sqlman/dialect/SQLiteDialect.java
@@ -6,6 +6,7 @@ import io.sqlman.SqlDialect;
 import io.sqlman.SqlVersion;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 /**
@@ -22,7 +23,32 @@ public class SQLiteDialect implements SqlDialect {
 
     @Override
     public void install(Connection connection, SqlConfig config) throws SQLException {
-
+        String name = config.getTableName();
+        {
+            StringBuilder ddl = new StringBuilder();
+            ddl.append(" CREATE TABLE IF NOT EXISTS ").append(name).append(" (");
+            ddl.append("     id integer NOT NULL PRIMARY KEY AUTOINCREMENT,");
+            ddl.append("     version varchar (24) NOT NULL,");
+            ddl.append("     ordinal int (11) NOT NULL,");
+            ddl.append("     description varchar (128) NOT NULL,");
+            ddl.append("     sql_quantity int (11) NOT NULL,");
+            ddl.append("     success bit (1) NOT NULL,");
+            ddl.append("     row_effected int (11) NOT NULL,");
+            ddl.append("     error_code int (11) NOT NULL,");
+            ddl.append("     error_state varchar (255) NOT NULL,");
+            ddl.append("     error_message varchar (255) NOT NULL,");
+            ddl.append("     time_executed timestamp NOT NULL DEFAULT (");
+            ddl.append("         datetime('now', 'localtime')");
+            ddl.append("     )");
+            ddl.append(" )");
+            PreparedStatement statement = connection.prepareStatement(ddl.toString());
+            statement.execute();
+        }
+        {
+            String sql = "CREATE UNIQUE INDEX UK_" + name + "_version_ordinal ON " + name + " (version DESC, ordinal DESC)";
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.execute();
+        }
     }
 
     @Override
