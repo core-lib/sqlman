514e72c1dc12aa20c37912263dafff20e122a550
core-lib <646742615>
2019-05-27 13:42:26 +0800

    增加日志记录器接口

--- src/main/java/io/sqlman/SqlLogger.java
+++ src/main/java/io/sqlman/SqlLogger.java
@@ -8,48 +8,168 @@ package io.sqlman;
  */
 public interface SqlLogger {
 
+    /**
+     * 获取日志记录器名称
+     *
+     * @return 日志记录器名称
+     */
     String getName();
 
+    /**
+     * 获取日志级别
+     *
+     * @return 日志级别
+     */
     SqlLogger.Level getLevel();
 
+    /**
+     * 判断 Trace 日志级别是否开启
+     *
+     * @return 开启则返回{@code true} 否则返回{@code false}
+     */
     boolean isTraceEnabled();
 
+    /**
+     * trace 级别日志输出
+     *
+     * @param msg 消息
+     */
     void trace(String msg);
 
+    /**
+     * trace 级别日志输出
+     *
+     * @param format    消息格式
+     * @param arguments 消息参数
+     */
     void trace(String format, Object... arguments);
 
+    /**
+     * trace 级别日志输出
+     *
+     * @param msg 消息
+     * @param t   异常
+     */
     void trace(String msg, Throwable t);
 
+    /**
+     * 判断 Debug 日志级别是否开启
+     *
+     * @return 开启则返回{@code true} 否则返回{@code false}
+     */
     boolean isDebugEnabled();
 
+    /**
+     * debug 级别日志输出
+     *
+     * @param msg 消息
+     */
     void debug(String msg);
 
+    /**
+     * debug 级别日志输出
+     *
+     * @param format    消息格式
+     * @param arguments 消息参数
+     */
     void debug(String format, Object... arguments);
 
+    /**
+     * debug 级别日志输出
+     *
+     * @param msg 消息
+     * @param t   异常
+     */
     void debug(String msg, Throwable t);
 
+    /**
+     * 判断 Info 日志级别是否开启
+     *
+     * @return 开启则返回{@code true} 否则返回{@code false}
+     */
     boolean isInfoEnabled();
 
+    /**
+     * info 级别日志输出
+     *
+     * @param msg 消息
+     */
     void info(String msg);
 
+    /**
+     * info 级别日志输出
+     *
+     * @param format    消息格式
+     * @param arguments 消息参数
+     */
     void info(String format, Object... arguments);
 
+    /**
+     * info 级别日志输出
+     *
+     * @param msg 消息
+     * @param t   异常
+     */
     void info(String msg, Throwable t);
 
+    /**
+     * 判断 Warn 日志级别是否开启
+     *
+     * @return 开启则返回{@code true} 否则返回{@code false}
+     */
     boolean isWarnEnabled();
 
+    /**
+     * warn 级别日志输出
+     *
+     * @param msg 消息
+     */
     void warn(String msg);
 
+    /**
+     * warn 级别日志输出
+     *
+     * @param format    消息格式
+     * @param arguments 消息参数
+     */
     void warn(String format, Object... arguments);
 
+    /**
+     * warn 级别日志输出
+     *
+     * @param msg 消息
+     * @param t   异常
+     */
     void warn(String msg, Throwable t);
 
+    /**
+     * 判断 Error 日志级别是否开启
+     *
+     * @return 开启则返回{@code true} 否则返回{@code false}
+     */
     boolean isErrorEnabled();
 
+    /**
+     * error 级别日志输出
+     *
+     * @param msg 消息
+     */
     void error(String msg);
 
+    /**
+     * error 级别日志输出
+     *
+     * @param format    消息格式
+     * @param arguments 消息参数
+     */
     void error(String format, Object... arguments);
 
+    /**
+     * error 级别日志输出
+     *
+     * @param msg 消息
+     * @param t   异常
+     */
     void error(String msg, Throwable t);
 
     /**
